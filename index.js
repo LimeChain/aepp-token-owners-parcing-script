bc20b41d01415a8fbe306b423a33412612fe3218
Aleksey <alexeitcekov@abv.bg>
2019-09-27 14:05:46 +0300

    edit http request to backend

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 const fs = require('fs');
 const axios = require('axios');
 
-const tokenOwnerDataPath = "./data/db-eth-node-sorted.json";
-const BACKEND_URL = "https://token-migration.appspot.com";
-const REQUEST_INTERVAL = 10; // ms
+// const tokenOwnerDataPath = "./data/db-eth-node-sorted.json";
+const tokenOwnerDataPath = "./data/infura.json";
 
-const AUTH_TOKEN = "YOUR_TOKEN_HERE";
+// const SEED_URL = "https://token-migration.appspot.com/owner";
+const SEED_URL = 'http://localhost:3000/owner';
 
-(async function() {
+(async function () {
     let data = JSON.parse(fs.readFileSync(tokenOwnerDataPath, 'utf8'));
     const ethAddresses = Object.keys(data);
 
-    let index = 1; // index 0 is already inserted
     const iterations = ethAddresses.length;
 
-    let sendInterval = setInterval(async function () {
-        try {
-
-            if (index >= iterations) {
-                clearInterval(sendInterval);
-                return;
-            }
-            
-            var config = {
-                headers: {'Authorization': "Bearer " + AUTH_TOKEN}
-            };
-
-            // send to 
-            axios.post(BACKEND_URL + "/owner", {
-                ethAddress: ethAddresses[index],
-                balance: data[ethAddresses[index]]
-            }, config).then(result => {
-                console.log(result.data ? result.data : result);
-                console.log();
-            })
-
-            
-            index++;
-        } catch (error) {
-            console.log(error.data ? error.data : error);
-            clearInterval(sendInterval);
+    var config = {
+        headers: {
+            'Authorization': "Bearer 9015e355-8dec-49a5-a5a6-70a51cc9f2d1"
         }
-    }, REQUEST_INTERVAL)
+    };
+
+    for(let prop in data) {
+        if (!prop || prop == '') {
+            console.log("===> EMPTY address");
+            continue
+        }
+
+        // send to 
+        let result = await axios.post(SEED_URL, {
+            ethAddress: prop,
+            balance: data[prop]
+        }, config)
+
+        console.log();
+        console.log(result.data);
+    }
 })();
